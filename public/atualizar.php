a76e9a9cb9de0b13ac0903d96f6f6537c36ac69c
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-08-12 18:23:44 -0300

    up - modificação no atualizar

--- public/atualizar.php
+++ public/atualizar.php
@@ -7,7 +7,9 @@ $titulo = $_POST["titulo"];
 $autor = $_POST["autor"];
 $ano = $_POST["ano"];
 
-$sql = "UPDATE livros SET titulo='$titulo',autor='$autor',ano='$ano' WHERE id = '$id'";
+$sql = "UPDATE livros SET titulo=?,autor=?,ano=? WHERE id = ?";
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("ssii", $titulo, $autor, $ano, $id);
+$stmt->execute();
 
-mysqli_query($conexao, $sql);
 header("Location: ../index.php");
