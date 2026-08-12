bb64a2732aac8beb75040ae20df8d21d533670a6
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-08-12 18:10:41 -0300

    up - modificação no excluir

--- public/excluir.php
+++ public/excluir.php
@@ -1,7 +1,11 @@
 <?php
 include "../infra/conexao.php";
 $id = $_GET["id"];
-$sql = "DELETE FROM livros WHERE id=$id";
-mysqli_query($conexao,$sql);
+
+$sql = "DELETE FROM livros WHERE id=?";
+$stmt = $conexao->prepare($sql); //prepara a consulta no banco
+$stmt->bind_param("i", $id); // liga a variavel ao ? e i = numero inteiro
+$stmt->execute();
+
 header("Location: ../index.php");
 ?>
