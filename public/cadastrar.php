f2ed2ed21132af3f1039ade630c488fff213b29b
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-08-12 17:50:04 -0300

    up - modificação no cadastro

--- public/cadastrar.php
+++ public/cadastrar.php
@@ -6,9 +6,11 @@ $titulo = $_POST["titulo"];
 $autor = $_POST["autor"];
 $ano = $_POST["ano"];
 
-$sql = "INSERT INTO livros (titulo,autor,ano) VALUES ('$titulo','$autor','$ano')";
-
-mysqli_query($conexao, $sql);
+// query com ? no lugar dos valores
+$sql = "INSERT INTO livros (titulo,autor,ano) VALUES (?,?,?)";
+$stmt = $conexao->prepare($sql); // prepara a consulta no banco
+$stmt->bind_param("ssi", $titulo, $autor, $ano); // liga cada variavel a um ?
+$stmt->execute(); // os dados são enviados
 
 header("Location: ../index.php");
 ?>
